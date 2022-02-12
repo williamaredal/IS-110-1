54df80f0a5df95d740ef9040cc6fb61491f39254
William Peter Åredal <williamaredal@hotmail.com>
2022-02-12 12:47:31 +0100

    Pokedex

---
+++ Modul4/Pokedex.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+/**
+ * A pokedex for keeping track of all your pokemons.
+ *
+ * @author William
+ * @version 1
+ */
+public class Pokedex
+{
+    private ArrayList<Pokemon> myPokedex;
+
+    /**
+     * Constructor for objects of class Pokedex
+     */
+    public Pokedex()
+    {
+        myPokedex = new ArrayList<Pokemon>();
+    }
+
+    /**
+     * A method for adding a pokemon to myPokeDex
+     *
+     * @param  name,pokemonType,lvl the fields of the pokemon
+     * @adds the new pokemon to myPokedex
+     */
+    public void addPokemon(String name, String pokemonType, int lvl)
+    { 
+        this.myPokedex.add(new Pokemon(name, pokemonType, lvl));
+        System.out.println("The pokemon was added to your pokedex");
+    }
+}
