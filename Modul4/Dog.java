7d4ade525dafc459e139402a628f81c12d998640
William Peter Åredal <williamaredal@hotmail.com>
2022-04-27 15:37:23 +0200

    correction to include field checking

--- Modul4/Dog.java
+++ Modul4/Dog.java
@@ -33,7 +33,17 @@ public class Dog extends Animal
     }
 
     public boolean equals(Object compareObject) {
-        return this.getClass() == compareObject.getClass();
+        // the object is the same as this
+        if (this == compareObject) {
+            return true;
+        }
+        if (compareObject instanceof Dog) {
+            // gets fields from comparison object and returns true if fields contain the same
+            Dog other = (Dog) compareObject;
+            return name.equals(other.name);
+        } else {
+            return false;
+        }
     }
 
     public int hashCode(Object compareObject) {
